1f4a218c2ffc061dc02bb1a7c253ea2f397e9d54
Qube0d <smetacometa@gmail.com>
2022-09-25 22:44:12 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const mario = document.getElementById("mario");
+const pipe = document.getElementById("pipe");
+
+
+document.addEventListener("keydown", function (event) {
+    jump();
+});
+function jump() {
+    console.log("jump")
+    if(mario.classList != "jump"); {
+        mario.classList.add("jump")
+    }
+    setTimeout(function () {
+     mario.classList.remove("jump")  
+   }, 300)
+}
+
+let isAlive = setInterval(function () {
+    let marioTop = parseInt(window.getComputedStyle(mario).getPropertyValue("top"));
+    let pipeLeft = parseInt(window.getComputedStyle(pipe).getPropertyValue("left"));
+
+    if (pipeLeft < 50 && pipeLeft > 0 && marioTop >= 140) {
+        alert("Game over")
+    }
+}, 10)
